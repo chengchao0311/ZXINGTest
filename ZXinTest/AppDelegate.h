09966a1ddd902fd2a840f2188799000f3dfd1d0a
陈鑫琦 <chenxinqi@chens-macbook-pro.local>
2016-07-17 20:14:18 +0800

    Initial Commit

---
+++ ZXinTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXinTest
+//
+//  Created by 陈 鑫琦 on 7/17/16.
+//  Copyright © 2016 cxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXinTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXinTest
+//
+//  Created by 陈 鑫琦 on 7/17/16.
+//  Copyright © 2016 cxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
